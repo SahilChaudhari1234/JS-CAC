de518164aa056ebd16d259b26acd508801bb96f1
SahilChaudhari1234 <142141840+SahilChaudhari1234@users.noreply.github.com>
2025-10-16 18:23:08 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,52 @@
+const accountId = 12233
+let accountEmail = "Sahil@google.com"
+var password = "123456"
+city = "Wardha"
+let val
+arr = [1, 2, "Sahil",[1,3,4,5]]  //array
+obj = {name: "Sahil", age: 21, roll_no: 20}  //obj (use to represent the data which is iin form of key value pair)
+
+// accountId = 3  //not allowed
+
+console.log(accountId);
+
+accountEmail = "dev@google.com"
+password = "09876"
+city = "Hgt"
+//means var and let can be change
+
+/*prefer not to use var 
+because of issue in block scope and functional scope*/
+//but with let we didn't
+//to print more than one thing simultaneously
+console.table([accountId, accountEmail, password, city, val,obj])
+
+console.log(arr)
+console.log(obj)
+console.log(typeof(city)); //string
+
+
+
+
+
+console.log(`I am a frontend 
+developer`);
+
+//console.log("99"+"9"); //o/p: 999 (concatination)
+//console.log("99"+9); //o/p: 999 (concatination)
+//console.log(99+"9"); //o/p: 999 (concatination)
+//console.log("99"-9);   //o/p : 90 (simple substraction)
+//console.log(99-"9");   //o/p : 90 (simple substraction)
+//console.log("Sunil"-8);   //NaN  //not a number
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //to indicate that the JS code written here is of newer version 
+
+//alert(3+3)  //will not run here as we are using nodejs, not browser
+//documentation: mdn and tc39
+
+//number => range: 2 to power 53
+//bigint => for storing bigger numbers (uses in stock market or in e-commerce website)
+//string => ""
+//null => datatype and standalone value  //let s2 = null
+//undefined => variable declared but not assigned //let s2 //example if we create the s/w to send the present temp and if the sw is failed then it should return undefined not 0 as 0 is the temp 
+//boolean => true/false
+//symbol => to idntify the unique component in figma and react
+//object
+//null is a object, undefined has typeof as undefined
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+ 
